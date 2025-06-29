dc1b9bf2576573629780b83d1bc82143235c838e
NkemJefersonAchia <nkemachia05@gmail.com>
2025-06-29 21:08:17 +0200

    ok

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv[1:]
+    count = len(argv)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print(f"{count} arguments:")
+
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+This is the file for python-import_modules
